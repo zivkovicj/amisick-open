5c3177a1b986d0cd27a0fb13e8d32da2a23b6f9c
Thiago <thiago@crova.me>
2018-06-20 19:54:52 +0200

    Show code

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM ruby:2.5
+RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs
+RUN mkdir /amisick
+WORKDIR /amisick
+COPY amisick/Gemfile /amisick/Gemfile
+COPY amisick/Gemfile.lock /amisick/Gemfile.lock
+RUN bundle install
+COPY amisick /amisick
+EXPOSE 3000
+CMD [ "bundle", "exec", "puma", "-C", "config/puma.rb" ]
---
+++ amisick/app/models/m_webhook.rb
@@ -0,0 +1,27 @@
+class MWebhook < ApplicationRecord
+  # Scopes
+  # List of IPS => IP #1: 153.92.239.29 / IP #2 = 153.92.239.141
+  scope :ip_1, lambda { where("m_webhooks.sending_ip = ?", "153.92.239.29") }
+  scope :ip_2, lambda { where("m_webhooks.sending_ip = ?", "153.92.239.141") }
+  scope :ip_3, lambda { where("m_webhooks.sending_ip = ?", "213.32.165.6") }
+  scope :ip_4, lambda { where("m_webhooks.sending_ip = ?", "213.32.165.213") }
+  scope :ip_5, lambda { where("m_webhooks.sending_ip = ?", "153.92.251.118") }
+  scope :ip_6, lambda { where("m_webhooks.sending_ip = ?", "213.32.165.168") }
+  # Lists ESPS => 'gmail' / 'hotmail' / 'yahoo' / 'bol' / 'others'
+  scope :gmail, lambda { where("m_webhooks.esp LIKE ?", "gmail") }
+  scope :hotmail, lambda { where("m_webhooks.esp LIKE ?", "hotmail") }
+  scope :yahoo, lambda { where("m_webhooks.esp LIKE ?", "yahoo") }
+  scope :bol, lambda { where("m_webhooks.esp LIKE ?", "bol") }
+  scope :other, lambda { where("m_webhooks.esp NOT LIKE ?
+    AND m_webhooks.esp NOT LIKE ?
+    AND m_webhooks.esp NOT LIKE ?",
+    "gmail", "hotmail", "yahoo" ) }
+  # Ordering
+  scope :desc, lambda { order("m_webhooks.date_event DESC") }
+  # Lists Event types => "clicked", "opened"...
+  scope :opened, lambda { where("m_webhooks.event LIKE ?", "opened") }
+  # Limiting
+  scope :ten, lambda { limit (10)}
+
+  # end of Ruby Class
+end
